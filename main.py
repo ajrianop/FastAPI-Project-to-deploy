346885701ceb8a6662d5046ffe76cb6cfced8199
ajrianop <ajrianop@unal.edu.co>
2024-03-29 18:28:31 -0500

    Adding parameters and filter according to the path

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ app.version = "0.0.1"
 
 series = [
     {
+        "id" : 1,
         "Title": "Game of Thrones",
         "Genre": ["Fantasy", "Drama"],
         "Synopsis": "Based on George R.R. Martin's novels, it follows the power struggles among noble families as they vie for control of the Iron Throne of the Seven Kingdoms of Westeros.",
         "Main Cast": ["Emilia Clarke", "Kit Harington", "Peter Dinklage", "Lena Headey"]
     },
     {
+        "id" : 2,
         "Title": "Breaking Bad",
         "Genre": ["Crime", "Drama", "Thriller"],
         "Synopsis": "A high school chemistry teacher turned methamphetamine manufacturer partners with a former student to build a drug empire while dealing with personal and professional challenges.",
         "Main Cast": ["Bryan Cranston", "Aaron Paul", "Anna Gunn", "Dean Norris"]
     },
     {
+        "id" : 3,
         "Title": "Friends",
         "Genre": ["Comedy", "Romance"],
         "Synopsis": "Follows the lives, relationships, and comedic antics of six friends living in Manhattan.",
@@ -35,3 +38,10 @@ def message():
 @app.get('/series', tags = ['series'])
 def get_series():
     return series
+
+@app.get('/series/{id}', tags = ['series'])
+def get_movie(id : int):
+    for item in series:
+        if item["id"] == id:
+            return item
+    return []
