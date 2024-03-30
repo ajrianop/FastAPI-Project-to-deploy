8938814e6fde80503ec042cf6049e7744c83dc6b
ajrianop <ajrianop@unal.edu.co>
2024-03-29 19:11:48 -0500

    query parameters

--- main.py
+++ main.py
@@ -8,24 +8,30 @@ app.version = "0.0.1"
 series = [
     {
         "id" : 1,
-        "Title": "Game of Thrones",
-        "Genre": ["Fantasy", "Drama"],
-        "Synopsis": "Based on George R.R. Martin's novels, it follows the power struggles among noble families as they vie for control of the Iron Throne of the Seven Kingdoms of Westeros.",
-        "Main Cast": ["Emilia Clarke", "Kit Harington", "Peter Dinklage", "Lena Headey"]
+        "title": "Game of Thrones",
+        "genre": ["fantasy", "drama"],
+        "synopsis": "Based on George R.R. Martin's novels, it follows the power struggles among noble families as they vie for control of the Iron Throne of the Seven Kingdoms of Westeros.",
+        "main Cast": ["Emilia Clarke", "Kit Harington", "Peter Dinklage", "Lena Headey"],
+        "year start": 2011,
+        "year finalization": 2019
     },
     {
         "id" : 2,
-        "Title": "Breaking Bad",
-        "Genre": ["Crime", "Drama", "Thriller"],
-        "Synopsis": "A high school chemistry teacher turned methamphetamine manufacturer partners with a former student to build a drug empire while dealing with personal and professional challenges.",
-        "Main Cast": ["Bryan Cranston", "Aaron Paul", "Anna Gunn", "Dean Norris"]
+        "title": "Breaking Bad",
+        "genre": ["crime", "drama", "thriller"],
+        "synopsis": "A high school chemistry teacher turned methamphetamine manufacturer partners with a former student to build a drug empire while dealing with personal and professional challenges.",
+        "main Cast": ["Bryan Cranston", "Aaron Paul", "Anna Gunn", "Dean Norris"],
+        "year start": 2008,
+        "year finalization": 2013
     },
     {
         "id" : 3,
-        "Title": "Friends",
-        "Genre": ["Comedy", "Romance"],
+        "title": "Friends",
+        "genre": ["comedy", "romance"],
         "Synopsis": "Follows the lives, relationships, and comedic antics of six friends living in Manhattan.",
-        "Main Cast": ["Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "Matt LeBlanc", "Matthew Perry", "David Schwimmer"]
+        "main Cast": ["Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "Matt LeBlanc", "Matthew Perry", "David Schwimmer"],
+        "year start": 1994,
+        "year finalization": 2004
     },
 ]
 
@@ -41,7 +47,27 @@ def get_series():
 
 @app.get('/series/{id}', tags = ['series'])
 def get_movie(id : int):
-    for item in series:
-        if item["id"] == id:
-            return item
+    for serie in series:
+        if serie["id"] == id:
+            return serie
     return []
+
+@app.get('/series_one/', tags = ['series'])
+def get_serie_by_genre(genre : str):
+    for serie in series:
+        if genre in serie['genre']:
+            return serie
+    return ['There is no information']
+
+@app.get('/series_all/', tags = ['series'])
+def get_series_by_genre(genre : str):
+    series_filtered_genre = [serie for serie in series if genre in serie['genre']]
+    return series_filtered_genre
+'''
+@app.get('/series/', tags = ['series'])
+def get_series_by_year_of_start(year_start : int):
+    for item in series:
+        if str(item['year start']) == str(year_start):
+            return item
+    return ['There is no information']
+'''
